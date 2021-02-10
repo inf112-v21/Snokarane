fd9acce20fbc6625a26cba808c54c83050a402bd
PeterGillow <takenslangerarara@gmail.com>
2021-02-10 13:31:12 +0100

    Added a simple flag class (Coded in a pair programming session with Hallvard, Audun, Jørgen and Peter)

---
+++ src/test/java/inf112/skeleton/app/FlagTest.java
@@ -0,0 +1,16 @@
+package inf112.skeleton.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlagTest {
+
+    @Test
+    public void PlayerIsInitializedAtPosition00(){
+        Flag flag = new Flag(0, 1);
+
+        assertEquals(flag.getX(), 0);
+        assertEquals(flag.getY(), 1);
+    }
+}
