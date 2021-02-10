75cacf289525fb08b0abb3e5fdd1e339dd511ddf
PeterGillow <takenslangerarara@gmail.com>
2021-02-10 13:52:02 +0100

    Added a function to visit flags in player class (Coded in a pair programming session with Hallvard, Audun, Jørgen and Peter)

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -2,10 +2,15 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.math.GridPoint2;
 
+import java.util.*;
+
 public class Player {
 
     // Player position initialized at 0, 0
     private GridPoint2 position = new GridPoint2(0, 0);
+
+    // All flags visited
+    private List<Flag> flagsVisited = new ArrayList<>();
 
     public void move(int x, int y) {
         position.x+=x;
@@ -15,4 +20,19 @@ public class Player {
     public int getX() { return position.x; }
     public int getY() { return position.y; }
 
+    /**
+     * @param flag flag to visist
+     * @return true if flag hasn't been visisted before
+     */
+    public boolean visitFlag(Flag flag){
+        if (!flagsVisited.contains(flag)){
+            flagsVisited.add(flag);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Flag> getVisitedFlags() {
+        return flagsVisited;
+    }
 }
--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -2,7 +2,7 @@ package inf112.skeleton.app;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PlayerTest {
 
@@ -22,5 +22,24 @@ public class PlayerTest {
 
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 1);
+    }
+
+    @Test
+    public void visitFlagThatIsNotAlreadyVisited(){
+        Player player = new Player();
+        Flag flag = new Flag(2, 2);
+
+        assertTrue(player.visitFlag(flag));
+        assertEquals(player.getVisitedFlags().get(0), flag);
+    }
+
+    @Test
+    public void visitFlagThatHasBeenVisited(){
+        Player player = new Player();
+        Flag flag = new Flag(2, 2);
+        player.visitFlag(flag);
+
+        assertFalse(player.visitFlag(flag));
+        assertEquals(player.getVisitedFlags().size(), 1);
     }
 }
