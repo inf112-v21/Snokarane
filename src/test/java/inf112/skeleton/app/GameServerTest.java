69716c42cf878721df870a65dc4e9ab27042cec1
Jorgen <jorgen.lohne99@gmail.com>
2021-02-21 13:29:39 +0100

    Reformated file

--- src/test/java/inf112/skeleton/app/GameServerTest.java
+++ src/test/java/inf112/skeleton/app/GameServerTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class GameServerTest {
 
     @Test
-    public void isGameServerTest(){
+    public void isGameServerTest() {
         GameServer gameServer = new GameServer();
 
         assertTrue(gameServer.isHost);
