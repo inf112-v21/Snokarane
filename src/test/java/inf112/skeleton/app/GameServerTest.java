fcd6dea398c853fc4b6a64830262c51fac5d785f
Jorgen <jorgen.lohne99@gmail.com>
2021-02-21 12:55:00 +0100

    Created GameServerTest Added isGameServerTest

---
+++ src/test/java/inf112/skeleton/app/GameServerTest.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GameServerTest {
+
+    @Test
+    public void isGameServerTest(){
+        GameServer gameServer = new GameServer();
+
+        assertTrue(gameServer.isHost);
+
+
+    }
+
+
+}
