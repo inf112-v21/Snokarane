06d932a5e8937e2f166d5c219e85eb67f615a9d4
PeterGillow <takenslangerarara@gmail.com>
2021-02-24 12:53:24 +0100

    Updated MapTest to load "test-map.tmx". Not currently working.

--- src/test/java/inf112/skeleton/app/MapTest.java
+++ src/test/java/inf112/skeleton/app/MapTest.java
@@ -1,10 +1,29 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MapTest {
     Map map;
+    Player player;
+
+    TiledMap tiledMap;
+
+
+
+    @Before
+    public void init(){
+        TmxMapLoader mapLoader = new TmxMapLoader();
+        tiledMap = mapLoader.load("test-map.tmx");
+        map.loadMapLayers(tiledMap);
+        map = new Map();
+        player = new Player();
+    }
 
     @Test
     //Amazing test
@@ -12,5 +31,10 @@ public class MapTest {
         map = new Map();
         assertEquals(5, map.getBoardX());
         assertEquals(5, map.getBoardY());
+    }
+
+    @Test
+    public void atLeastOneFlagIsLoaded(){
+        assertNotEquals(0, map.flagPositions.size());
     }
 }
