51e8aa8337dcfbd7ccd57f3209f84f4a153757ae
Jorgen <jorgen.lohne99@gmail.com>
2021-02-26 19:06:39 +0100

    Created Card class with simple constructor

---
+++ src/main/java/inf112/skeleton/app/Card.java
@@ -0,0 +1,16 @@
+package inf112.skeleton.app;
+
+public class Card {
+    private CardType cardType;
+
+
+    public Card(CardType cardType){
+        this.cardType = cardType;
+    }
+
+
+
+    public CardType getCardType() {
+        return cardType;
+    }
+}
