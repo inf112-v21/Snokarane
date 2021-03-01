4b1aad4d6f28ca8786df8fbe079665777654fb88
Jorgen <jlo035@uib.no>
2021-03-01 21:15:23 +0100

    Implemented addCardToHand() to GameClient

--- src/main/java/inf112/skeleton/app/game/GameClient.java
+++ src/main/java/inf112/skeleton/app/game/GameClient.java
@@ -1,7 +1,19 @@
 package inf112.skeleton.app.game;
-
+import inf112.skeleton.app.game.objects.Card;
 import inf112.skeleton.app.network.NetworkClient;
+
+import java.util.ArrayList;
 
 public class GameClient extends GamePlayer {
     NetworkClient client = new NetworkClient();
+
+    ArrayList<Card> hand = new ArrayList<>();
+
+    public void addCardToHand(Card card){
+        hand.add(card);
+    }
+
+
+
+
 }
