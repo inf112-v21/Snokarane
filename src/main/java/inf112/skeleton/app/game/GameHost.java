7b28b67ad507513a3394ffd90efbead1ca8e4914
Jorgen <jlo035@uib.no>
2021-03-01 21:16:07 +0100

    Implemented handOutCard() to GameHost

--- src/main/java/inf112/skeleton/app/game/GameHost.java
+++ src/main/java/inf112/skeleton/app/game/GameHost.java
@@ -1,7 +1,13 @@
 package inf112.skeleton.app.game;
 
+import inf112.skeleton.app.game.objects.Card;
 import inf112.skeleton.app.network.NetworkHost;
 
 public class GameHost extends GamePlayer {
     NetworkHost host = new NetworkHost();
+
+    public Card handOutCard(Card card){
+        return card;
+    }
+
 }
