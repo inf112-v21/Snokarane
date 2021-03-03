22602beee8865daaefb19d0e0aedd7453227c4a6
Audun <audun.henriksen@gmail.com>
2021-03-03 17:58:47 +0100

    Changed the way move() works to account for player direction. Implemented a way to check where the player ends up if they move. Implemented a way to rotate the player.

--- src/main/java/inf112/skeleton/app/game/objects/PlayerToken.java
+++ src/main/java/inf112/skeleton/app/game/objects/PlayerToken.java
@@ -17,9 +17,19 @@ public class PlayerToken {
     //Current direction of player
     private Direction playerDirection;
 
-    public void move(int x, int y) {
-        position.x += x;
-        position.y += y;
+    /**
+     * Moves the player 1 step in the direction it is facing
+     */
+    public void move() {
+        moveDir(position, playerDirection);
+    }
+
+    /**
+     * Use this method to move a player when the player is being pushed by something.
+     * @param direction The direction you want to move, regardless of the players direction
+     */
+    public void move(Direction direction) {
+        moveDir(position, direction);
     }
 
     public int getX() {
@@ -49,6 +59,46 @@ public class PlayerToken {
 
     public Direction getDirection() {
         return playerDirection;
+    }
+
+    public GridPoint2 wouldEndUp() {
+        GridPoint2 newPos = new GridPoint2(position.x, position.y);
+        moveDir(newPos, playerDirection);
+        return newPos;
+    }
+
+    private void moveDir(GridPoint2 position, Direction direction) {
+        switch (direction) {
+            case NORTH:
+                position.x +=1;
+            case SOUTH:
+                position.x -=1;
+            case WEST:
+                position.y -= 1;
+            case EAST:
+                position.y += 1;
+        }
+    }
+
+    public void rotate(CardType rotationDirection) {
+        switch (playerDirection) {
+            case EAST:
+                if (rotationDirection == CardType.TURNLEFT) playerDirection = Direction.NORTH;
+                if (rotationDirection == CardType.TURNRIGHT) playerDirection = Direction.SOUTH;
+                if (rotationDirection == CardType.UTURN) playerDirection = Direction.WEST;
+            case WEST:
+                if (rotationDirection == CardType.TURNLEFT) playerDirection = Direction.SOUTH;
+                if (rotationDirection == CardType.TURNRIGHT) playerDirection = Direction.NORTH;
+                if (rotationDirection == CardType.UTURN) playerDirection = Direction.EAST;
+            case NORTH:
+                if (rotationDirection == CardType.TURNLEFT) playerDirection = Direction.WEST;
+                if (rotationDirection == CardType.TURNRIGHT) playerDirection = Direction.EAST;
+                if (rotationDirection == CardType.UTURN) playerDirection = Direction.SOUTH;
+            case SOUTH:
+                if (rotationDirection == CardType.TURNLEFT) playerDirection = Direction.EAST;
+                if (rotationDirection == CardType.TURNRIGHT) playerDirection = Direction.WEST;
+                if (rotationDirection == CardType.UTURN) playerDirection = Direction.NORTH;
+        }
     }
 
     public enum Direction {
