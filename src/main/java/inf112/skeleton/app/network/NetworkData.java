fc1dea8df8b17d77512cee909f2be45adca1ae37
Audun <audun.henriksen@gmail.com>
2021-03-04 11:23:38 +0100

    Forgot to register the PlayerToken for KryoNet

--- src/main/java/inf112/skeleton/app/network/NetworkData.java
+++ src/main/java/inf112/skeleton/app/network/NetworkData.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import inf112.skeleton.app.game.objects.Card;
 import inf112.skeleton.app.game.objects.CardType;
+import inf112.skeleton.app.game.objects.PlayerToken;
 import inf112.skeleton.app.libgdx.Map;
 import inf112.skeleton.app.libgdx.MapLayerWrapper;
 import inf112.skeleton.app.libgdx.NetworkDataWrapper;
@@ -36,47 +37,13 @@ public class NetworkData {
     // I think this should be here?
     public static List<Class> classesToRegister() {
         List<Class> classesToRegister = new ArrayList<>();
-        classesToRegister.add(TiledMap.class);
         classesToRegister.add(Card.class);
         classesToRegister.add(cardList.class);
         classesToRegister.add(ArrayList.class);
         classesToRegister.add(CardType.class);
-        classesToRegister.add(MapLayers.class);
-        classesToRegister.add(Map.class);
-        classesToRegister.add(TiledMapTileLayer.class);
+        classesToRegister.add(PlayerToken.class);
         classesToRegister.add(NetworkDataWrapper.class);
-        classesToRegister.add(TiledMapTileLayer.Cell.class);
-        classesToRegister.add(TiledMapTileLayer.Cell[][].class);
-        classesToRegister.add(TiledMapTileLayer.Cell[].class);
-        classesToRegister.add(StaticTiledMapTile.class);
-        classesToRegister.add(TiledMapTile.class);
-        classesToRegister.add(TiledMapTile.BlendMode.class);
-        classesToRegister.add(TextureRegion.class);
-        classesToRegister.add(Texture.class);
-        classesToRegister.add(Texture.TextureFilter.class);
-        classesToRegister.add(Texture.TextureWrap.class);
-        classesToRegister.add(FileTextureData.class);
-        classesToRegister.add(Lwjgl3FileHandle.class);
 
-        // This is starting to seem like a bad idea...
-        classesToRegister.add(java.io.File.class);
-        classesToRegister.add(Files.FileType.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.Camera.class);
-
-        // Do I need all these? Probably not. Am I sick of doing them one
-        // by one? Absolutely.
-        classesToRegister.add(Pixmap.Format.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.Color.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.Colors.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.Cubemap.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.FPSLogger.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.GLTexture.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.Mesh.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.VertexAttributes.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.TextureArray.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.PixmapIO.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.PerspectiveCamera.class);
-        classesToRegister.add(com.badlogic.gdx.graphics.OrthographicCamera.class);
         return classesToRegister;
     }
 }
