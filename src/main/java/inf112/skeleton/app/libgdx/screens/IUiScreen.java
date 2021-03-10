3c71f0b92845a650c40f23d53e9906ba11fa5b1c
PeterGillow <takenslangerarara@gmail.com>
2021-03-10 22:41:02 +0100

    Made an interface to use for menu screens with intractable items.

---
+++ src/main/java/inf112/skeleton/app/libgdx/screens/IUiScreen.java
@@ -0,0 +1,39 @@
+package inf112.skeleton.app.libgdx.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import inf112.skeleton.app.libgdx.RoboGame;
+
+/**
+ * Generic interface for UI Screens with menus, buttons, text fields and pictures etc.
+ */
+public interface IUiScreen extends Screen {
+    // RoboGame class
+    RoboGame game = new RoboGame();
+    // Stage for UI items
+    Stage stage = new Stage(new ScreenViewport());
+
+    // Screen width + height
+    float gdxW = 0;
+    float gdxH = 0;
+
+    /**
+     * Set stage, and set game to arg variable
+     * Initialize screen width and height,
+     * and call loadUIVisuals and loadUIIntreractibles
+     *
+     * @param game game to be passed into this.game
+     */
+    void startScreen(RoboGame game);
+
+    /**
+     * Load visual UI elements on screen
+     */
+    void loadUIVisuals();
+
+    /**
+     * Load intractable UI elements on screen
+     */
+    void loadUIIntractables();
+}
