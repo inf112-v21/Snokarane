722b6e25b97f885e67bfe47a95bed03e49cb53f3
hallv <hallvard.kobbeltveit@gmail.com>
2021-03-11 13:31:27 +0100

    added priorty and sorting of priority - pairprogramming with Audun

--- src/main/java/inf112/skeleton/app/game/objects/Card.java
+++ src/main/java/inf112/skeleton/app/game/objects/Card.java
@@ -1,7 +1,10 @@
 package inf112.skeleton.app.game.objects;
 
+import java.util.Comparator;
+
 public class Card {
     private CardType cardType;
+    private int priority;
 
     public Card(){
     }
@@ -12,4 +15,18 @@ public class Card {
     public CardType getCardType() {
         return cardType;
     }
+    public void setPriority(int newPriority) {this.priority = newPriority;}
+    public int getPriority() {
+        return priority;
+    }
+
+
+    public static class cardComparator implements Comparator<Card> {
+
+        @Override
+        public int compare(Card card1, Card card2) {
+            return card2.getPriority()-card1.getPriority();
+        }
+    }
+
 }
