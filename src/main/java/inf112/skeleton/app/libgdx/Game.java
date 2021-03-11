da83c0d809209b3a817b95133a8ddd7abe751f17
PeterGillow <takenslangerarara@gmail.com>
2021-03-11 14:38:02 +0100

    Added some javadocs to Game to explain a little better why it exists.

--- src/main/java/inf112/skeleton/app/libgdx/Game.java
+++ src/main/java/inf112/skeleton/app/libgdx/Game.java
@@ -32,6 +32,13 @@ import java.util.List;
 /**
  * Handles rendering, textures and event handling (key presses)
  * Game(this class) runs client side on each players PC, so only objects and methods necessary to have client side should be used here
+ *
+ * ---------------------------------------------
+ * DEPRECATED
+ * ---------------------------------------------
+ * This class has been moved to GameScreen class, as GameScreen is the class launched from the new UI.
+ * This class is still being kept here for a little while due to backwards compability.
+ * Check if it can be safely removed at some point.
  */
 public class    Game extends InputAdapter implements ApplicationListener {
     // For rendering text to screen
