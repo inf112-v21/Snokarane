2a6b0a15c77a5d1459bb69d1163866fe4c429490
PeterKrooni <peter.gillow.krooni@gmail.com>
2021-03-22 12:37:36 +0100

    Added UI package with handles

--- src/main/java/inf112/skeleton/app/ui/chat/backend/Message.java
+++ src/main/java/inf112/skeleton/app/ui/chat/backend/Message.java
@@ -1,18 +1,19 @@
-package inf112.skeleton.app.ui.chat;
+package inf112.skeleton.app.ui.chat.backend;
 
 public class Message {
     public String message;
-    public IChatter sender;
+    public ChatterData sender;
     public int messageID;
 
-    public Message(String message, IChatter sender){
-
+    public Message(String message, ChatterData sender){
+        this.message = message;
+        this.sender = sender;
     }
 
     // No args constructor required for kryo
     public Message(){}
 
-    public IChatter getSender(){
+    public ChatterData getSender(){
         return sender;
     }
 
