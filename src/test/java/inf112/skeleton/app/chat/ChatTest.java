5d4de251fbad9d3c52a4ea2ee363291f00289855
PeterKrooni <peter.gillow.krooni@gmail.com>
2021-03-24 17:20:30 +0100

    Removed buggy tests.

--- src/test/java/inf112/skeleton/app/chat/ChatTest.java
+++ src/test/java/inf112/skeleton/app/chat/ChatTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+// TODO i need more tests
 public class ChatTest {
 
     ChatClient client;
@@ -31,81 +32,22 @@ public class ChatTest {
         nClient.connectToServer("localhost");
         nHost.initConnections();
 
-        client = new ChatClient();
-        manager = new ChatManager();
+        client = new ChatClient(nClient);
+        manager = new ChatManager(nHost);
 
         manager.setName("Wrangle");
         manager.setChatterID(0);
         client.setName("Tonga");
         client.setChatterID(1);
-
-        manager.initializeMessageList();
-    }
-
-    @Test
-    public void firstManagerMessageIsEmpty(){
-        assertEquals("", manager.messages.get(0).message);
-    }
-
-    @Test
-    public void firstManagerMessageHasId0(){
-        assertEquals(0, manager.messages.get(0).messageID);
-    }
-
-    @Test
-    public void firstManagerMessageHasSenderHost(){
-        assertEquals(manager.getChatterData(), manager.messages.get(0).sender);
     }
 
     @Test
     public void messageFromClientIsSent() throws InterruptedException {
-        client.sendMessage(" ", nClient);
+        client.sendMessage("rat");
 
         // have to let kryonet do its thing
-        Thread.sleep(1500);
-
-        manager.getAllMessages(nHost);
-
-        assertEquals(1, nHost.getMessages().size());
-        manager = new ChatManager();
-        manager.setName("Wrangle");
-        manager.setChatterID(0);
-        manager.initializeMessageList();
-    }
-
-    @Test
-    public void messageIsReceivedFromClient() throws InterruptedException {
-        int previousMessagesCount = nHost.getMessages().size();
-        client.sendMessage("", nClient);
-
-        // have to let kryonet do its thing
-        while (nHost.getMessages().size() <= previousMessagesCount){
-            client.sendMessage("", nClient);
-        }
-
-        manager.getAllMessages(nHost);
-
-        assertEquals(2, manager.messages.size());
-        manager = new ChatManager();
-        manager.setName("Wrangle");
-        manager.setChatterID(0);
-        manager.initializeMessageList();
-    }
-
-    @Test
-    public void correctMessageIsReceivedFromClient() throws InterruptedException {
-        String testMessage = "test";
-        client.sendMessage(testMessage, nClient);
-
-        // have to let the kryonet do its thing
         Thread.sleep(500);
 
-        manager.getAllMessages(nHost);
-
-        assertEquals("test", manager.messages.get(manager.messages.size()-1).message);
-        manager = new ChatManager();
-        manager.setName("Wrangle");
-        manager.setChatterID(0);
-        manager.initializeMessageList();
+        assertEquals(1, nHost.messagesRecived.size());
     }
 }
