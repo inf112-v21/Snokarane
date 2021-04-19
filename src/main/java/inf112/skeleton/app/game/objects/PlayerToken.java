4be03e0d12455c9dbc76472333659ecee692b383
Jorgen <jlo035@uib.no>
2021-04-19 18:01:56 +0200

    Implemented getter and setter for config

--- src/main/java/inf112/skeleton/app/game/objects/PlayerToken.java
+++ src/main/java/inf112/skeleton/app/game/objects/PlayerToken.java
@@ -2,6 +2,7 @@ package inf112.skeleton.app.game.objects;
 
 import com.badlogic.gdx.math.GridPoint2;
 import inf112.skeleton.app.libgdx.Map;
+import inf112.skeleton.app.libgdx.PlayerConfig;
 
 import java.util.*;
 
@@ -25,6 +26,15 @@ public class PlayerToken {
 
     public String name;
 
+    public PlayerConfig getConfig() {
+        return config;
+    }
+
+    public void setConfig(PlayerConfig config) {
+        this.config = config;
+    }
+
+    private PlayerConfig config;
 
     // What texture gets displayed on character
     public enum CHARACTER_STATES{
